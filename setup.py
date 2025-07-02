2b49e0c196f84e70cb4d446df0234ae9153c45f7
anwin-sanju <dev.anwinsanju@gmail.com>
2025-07-02 19:41:54 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Anwin Sanju',
+    author_email= 'dev.anwinsanju@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
